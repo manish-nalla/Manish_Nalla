0f04f361a459061682c527d3dc8c83414d6a9b23
Manish <19luckyreddy@gmail.com>
2025-02-11 02:58:20 -0500

    Project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,31 @@
 import './Projects.css';
+import expenseImage from '../../Assets/expense-tracker-app.png';
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () =>{
+
+    const projects_array =[
+        {id:1, name:"Expense Tracker", technologies: 'React, JavaScript', icon: <img src={expenseImage} alt='Expense_Tracker' width={180}/>, github: 'https://github.com/manish-nalla/expense_tracker', live: "https://trackyourspends.netlify.app/"},
+        {id:2, name:"Expense Tracker", technologies: 'React, JavaScript', icon: <img src={expenseImage} alt='Expense_Tracker' width={180}/>, github: '', live: ""},
+        {id:3, name:"Expense Tracker", technologies: 'React, JavaScript', icon: <img src={expenseImage} alt='Expense_Tracker' width={180}/>, github: '', live: ""}
+    ]
+
     return (
         <div className='projects_container' id='projects'>
             <h1 className='heading'>Projects</h1>
-            <p className="skills_desc">Projects coming soon</p>
+            <div className="card_container">
+                    {projects_array.map((project)=>(
+                        <div className="card" key={project.id}>
+                            <div className="project_icon">{project.icon}</div>
+                            <div className="project_name">{project.name}</div>
+                            <div className="project_technologies">{project.technologies}</div>
+                            <div className="link_btn">
+                                <button ><a href={project.github} target='_blank' rel="noopener noreferrer"><FaGithub size={20}/></a></button>
+                                <button><a href={project.live} target='_blank' rel='noopener noreferrer'>Live Demo</a></button>
+                            </div>
+                        </div>
+                    ))}
+            </div>
         </div>
     );
 }
